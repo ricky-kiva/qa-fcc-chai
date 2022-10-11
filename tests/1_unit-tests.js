a0e466de0ceb5eec81807b82cff019d817049345
vento <r.ckyslash@gmail.com>
2022-10-11 22:50:31 +0700

    Change unit null

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,3 +1,5 @@
+// To test in TDD >> mocha --ui tdd fileName.js
+
 const chai = require('chai');
 const assert = chai.assert;
 
@@ -5,8 +7,8 @@ suite('Unit Tests', function () {
   suite('Basic Assertions', function () {
     // #1
     test('#isNull, #isNotNull', function () {
-      assert.fail(null, 'This is an optional error description - e.g. null is null');
-      assert.fail(1, '1 is not null');
+      assert.isNull(null, 'This is an optional error description - e.g. null is null');
+      assert.isNotNull(1, '1 is not null');
     });
     // #2
     test('#isDefined, #isUndefined', function () {
